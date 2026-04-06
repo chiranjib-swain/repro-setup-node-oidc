3c2012b47de7432ffe6a07e833012275cd2c79fb
chiranjib-swain <chiranjib-swain@github.com>
2026-04-06 17:53:45 +0530

    Downgrade Node.js version to 20 in workflow configuration; add greet function to index.js that includes version information

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = 'ok';
+const { version } = require('./package.json');
+
+function greet(name) {
+  return `Hello, ${name}! Running version ${version}`;
+}
+
+module.exports = { version, greet };
