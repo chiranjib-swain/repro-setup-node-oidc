d933a6041d17de42b49f39467b141cf3bf4c9d41
chiranjib-swain <chiranjib-swain@github.com>
2026-05-15 07:53:35 +0530

    Bump version to 0.0.30, update greeting message, and enable package manager cache in workflow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { version } = require('./package.json');
 
 function greet(name) {
-  return `Hello, ${name}! Running version ${version} new version`;
+  return `Hello, ${name}! Running version ${version} new version space`;
 }
 
 module.exports = { version, greet };
