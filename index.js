a54c0cee9b21f9250dfa08a94b2092eaf6a76c7f
chiranjib-swain <chiranjib-swain@github.com>
2026-05-15 15:25:52 +0530

    Bump version to 0.0.38, update greeting message in index.js, and adjust permissions in npm publish workflow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { version } = require('./package.json');
 
 function greet(name) {
-  return `Hello, ${name}! Running version ${version} new version space 0.0.3-beta.1.`;
+  return `Hello, ${name}! Running version ${version} new version space 0.0.38`;
 }
 
 module.exports = { version, greet };
