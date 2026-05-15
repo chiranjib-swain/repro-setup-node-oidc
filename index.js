55d222121583107781646a64ddad514b0d8d168d
chiranjib-swain <chiranjib-swain@github.com>
2026-05-15 13:35:55 +0530

    Bump version to 0.0.1-beta.1, update greeting message in index.js, and modify npm publish workflow for version tagging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { version } = require('./package.json');
 
 function greet(name) {
-  return `Hello, ${name}! Running version ${version} new version space 37.`;
+  return `Hello, ${name}! Running version ${version} new version space 0.0.1-beta.1.`;
 }
 
 module.exports = { version, greet };
