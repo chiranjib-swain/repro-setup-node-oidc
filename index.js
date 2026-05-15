e3bb19b82d025dc91afa40fd398d24bfd35010c5
chiranjib-swain <chiranjib-swain@github.com>
2026-05-15 13:52:28 +0530

    Bump version to 0.0.2-beta.1, update greeting message in index.js, and disable package manager cache in npm publish workflow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { version } = require('./package.json');
 
 function greet(name) {
-  return `Hello, ${name}! Running version ${version} new version space 0.0.1-beta.1.`;
+  return `Hello, ${name}! Running version ${version} new version space 0.0.2-beta.1.`;
 }
 
 module.exports = { version, greet };
