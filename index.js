8a2683c87a1727ddd6b06f0a896215ea0b804dfb
chiranjib-swain <chiranjib-swain@github.com>
2026-05-15 11:46:37 +0530

    Bump version to 0.0.36, update greeting message in index.js, and adjust cache settings in workflow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { version } = require('./package.json');
 
 function greet(name) {
-  return `Hello, ${name}! Running version ${version} new version space 35.`;
+  return `Hello, ${name}! Running version ${version} new version space 36.`;
 }
 
 module.exports = { version, greet };
